8ed4c70f2b6267ea0baed6f6f8d5a2220c1665c9
carlos <carlos@DESKTOP-J61L43O.mshome.net>
2021-07-29 09:59:24 +0200

    novedades

--- src/main/java/com/example/demo/domain/Car.java
+++ src/main/java/com/example/demo/domain/Car.java
@@ -1,5 +1,6 @@
 package com.example.demo.domain;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -14,6 +15,7 @@ public class Car {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	
+	@Column(name="fabricante")
 	private String manufacturer;
 	private String model;
 	private Double cc;
---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+insert into cars (fabricante, model, cc, doors) VALUES ('Opel','Corsa', 1.5, 5)
+insert into cars (fabricante, model, cc, doors) VALUES ('Ford','Mondeo', 1.8, 3)
+insert into cars (fabricante, model, cc, doors) VALUES ('Toyota','Prius', 2.0, 3)
