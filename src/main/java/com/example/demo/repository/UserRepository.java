3d743f0ca2e95dd5ee43f4f500c39b0c05a10c2a
carlos <carlos@DESKTOP-J61L43O.mshome.net>
2021-07-30 14:11:45 +0200

    viernes hora final

--- src/main/java/com/example/demo/repository/UserRepository.java
+++ src/main/java/com/example/demo/repository/UserRepository.java
@@ -11,4 +11,8 @@ import com.example.demo.domain.User;
 public interface UserRepository extends JpaRepository<User, Long> {
 	
 	Optional<User> findByUsername(String username);
+	
+	Boolean existsByUsername(String username);
+
+	Boolean existsByEmail(String email);
 }
